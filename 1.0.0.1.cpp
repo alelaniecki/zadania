fd97da20c4b2282beba53044962733f918bd34c1
alelaniecki <91740813+alelaniecki@users.noreply.github.com>
2021-10-22 15:18:47 +0200

    Add files via upload

---
+++ 1.0.0.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+auto main( ) -> int
+{
+
+int a,b,c;
+std::cout<<"podaj a: "<<std::endl;
+std::cin>>a;
+
+std::cout<<"podaj b: "<<std::endl;
+std::cin>>b;
+
+c=a+b;
+
+std::cout<<"twoj wynik"<<c;
+ return 0;
+}

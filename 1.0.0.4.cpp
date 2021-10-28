6f1610fbdfed79deb9cdb7d97a48df79e24ee111
alelaniecki <91740813+alelaniecki@users.noreply.github.com>
2021-10-28 18:43:00 +0200

    Add files via upload

---
+++ 1.0.0.4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int comparison(int a,int b)
+{
+	if(a>b)
+	{
+	return a;
+	}
+	else
+	return b;
+}
+
+int main()
+{
+	int a,b;
+	cout<<"podaj a"; cin>>a;
+	cout<<"podaj b"; cin>>b;
+	cout<<compraison(a,b)<<end1;
+	return 0;

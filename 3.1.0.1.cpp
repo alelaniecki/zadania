353fa189f9dcdfbf86af62d14f5048d50348c3a3
alelaniecki <91740813+alelaniecki@users.noreply.github.com>
2021-11-14 17:43:58 +0100

    Add files via upload

---
+++ 3.1.0.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int max=100;
+	int tab [max]={};
+	int n;
+	cout<<"podaj n";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<tab[i];
+	}
+	
+	return 0;
+}
